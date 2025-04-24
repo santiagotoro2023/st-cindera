0675bd640caa3e8632fde51608acfd0adce50b27
santiagotoro2023 <santiago.toro@sidmar.ch>
2025-04-25 00:05:18 +0200

    Initialized Repository and added existing files for the Cindera Productivity Tool.

---
+++ views/dashboard.view.php
@@ -0,0 +1,8 @@
+<?php
+require('views/partials/header.partial.php');
+require('views/partials/nav.partial.php');
+?>
+
+
+<?php require('dashboard.partial.php'); ?>
+<?php require('views/partials/footer.partial.php'); ?>
---
+++ views/partials/header.partial.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html class="screen-">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Cindera - <?= $heading ?> </title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
+        <link rel="stylesheet" href="recources/tailwind-styles.css" id="bs-base-css">
+        <link rel="icon" type="image/x-icon" href="recources/favicon.ico">
+    </head>
